6512a09447cdf1d2a9bc408f6a5350cc265fbb53
Matthee Kim <mattheekim@users.noreply.github.com>
2018-01-10 17:24:46 +0900

    first-commit
    first-commit

---
+++ functions.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Matthew on 10/01/2018.
+ */
+function nightDayHandler(self){
+    var target = document.querySelector('body');
+    if(self.value === 'night mode'){
+        target.style.backgroundColor = "black";
+        target.style.color = "white";
+        self.value = 'day mode'
+        var alist = document.querySelectorAll('a');
+        var i = 0;
+        while(i<alist.length){
+            alist[i].style.color = "powderblue";
+            i = i+1;
+        }
+    } else {
+        target.style.backgroundColor = 'white';
+        target.style.color = 'black';
+        self.value = 'night mode';
+        var alist = document.querySelectorAll('a');
+        var i = 0;
+        while(i<alist.length){
+            alist[i].style.color = 'blue';
+            i = i+1;
+        }
+    }
+}
